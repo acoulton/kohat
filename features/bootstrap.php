bd066e7401ac0f52a223cbbe5635a43559a204b4
Andrew Coulton <andrewnfcoulton@gmail.com>
2012-04-04 17:45:08 +0100

    Restructure folder layout and include all Behat contexts dynamically

--- features/bootstrap.php
+++
@@ -1,5 +0,0 @@
-<?php
-require_once __DIR__.'/../../../modules/unittest/bootstrap.php';
-
-// Attach a memory config reader
-Kohana::$config->attach(new Config_Memory);
---
+++ features/bootstrap/bootstrap.php
@@ -0,0 +1,17 @@
+<?php
+
+// Start off by requiring the Kohana unittest bootstrap as a good starting point
+require_once __DIR__.'../../../unittest/bootstrap.php';
+
+/*
+ * Include contexts from all levels of the application - you still have to
+ * manually register the ones you want in your application main context though.
+ */
+$contexts = Kohana::list_files('features/contexts/');
+foreach ($contexts as $context)
+{
+	if (pathinfo($context, PATHINFO_EXTENSION) == 'php')
+	{
+		require_once($context);
+	}
+}
