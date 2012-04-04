50125921eb81621cc304b7b4f564a10c582cb37e
Andrew Coulton <andrewnfcoulton@gmail.com>
2012-04-04 18:13:46 +0100

    Add the correct require path to unittest bootstrap and enable the kohat module

--- features/bootstrap/bootstrap.php
+++ features/bootstrap/bootstrap.php
@@ -1,12 +1,16 @@
 <?php
 
 // Start off by requiring the Kohana unittest bootstrap as a good starting point
-require_once __DIR__.'../../../unittest/bootstrap.php';
+require_once __DIR__.'/../../../unittest/bootstrap.php';
+
+// Enable the kohat module
+Kohana::modules(Kohana::modules() + array('kohat' => MODPATH.'kohat'));
 
 /*
  * Include contexts from all levels of the application - you still have to
  * manually register the ones you want in your application main context though.
  */
+
 $contexts = Kohana::list_files('features/contexts/');
 foreach ($contexts as $context)
 {
